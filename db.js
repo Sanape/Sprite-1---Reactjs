1aefc923117b8451993a61867b664fef7582f76e
unknown <sanape.sa@gmail.com>
2019-11-01 21:23:14 -0300

    add O to test

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const moongose = require('mongoose');
 mongoose.connect('mongodb+srv://JUAN:inetcom5@mlab-9hxhz.mongodb.net/test?retryWrites=true&w=majority',
     {
     useNewUrlParser: true,
-    useUnifiedTopology: true}).catch((err)=>{console.log('Error',err.message)})
+    useUnifiedTopology: true}).catch((err)=>{console.log('Erroor',err.message)})
 
     const db = mongoose.connection;
     module.exports = db
